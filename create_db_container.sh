ca9e71ae1ce2d7820b3d900a961263b33cdc7408
Arthur Stupin <arthur.stupin@veeam.com>
2024-06-02 21:48:19 +0300

    update test project

--- create_db_container.sh
+++ create_db_container.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "Запуск контейнера с БД"
+echo "Launch docker image with DB"
 cd /var/lib/jenkins/workspace/"project_name"/db_QAA && docker-compose up -d
 
-echo "Ожидаение поднятия контейнера"
+echo "Expilcit wait for DB"
 sleep 5
 
-echo "Накатываем бэкап БД для тестов"
+echo "Prepare test data"
 mysql -u login -ppassword --host=127.0.0.1 --port=33069 name_db < /var/lib/jenkins/workspace/"project_name"/db_QAA/dumps/example_dump.sql
--- drop_db_container.sh
+++ drop_db_container.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "Удаление содержимого схемы db"
+echo "Drop test DB"
 mysql -u login -ppassword --host=127.0.0.1 --port=33069 -e "drop database"
 
-echo "Создание пустой схемы"
+echo "Create clear schema for DB"
 mysql -u login -ppassword --host=127.0.0.1 --port=33069 -e "create database db_name character set utf8"
 
-echo "Стоп контейнер с БД"
+echo "Docker stop DB image"
 docker stop mariadb_10.1_QAA
